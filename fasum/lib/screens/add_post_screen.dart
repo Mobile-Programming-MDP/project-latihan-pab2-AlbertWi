93a62c0c0c8bd88a3199b85055341768dbcef023
AlbertWi <albertwijaya_2226240029@mhs.mdp.ac.id>
2025-04-25 20:02:50 +0700

    add_post_screen #2

--- fasum/lib/screens/add_post_screen.dart
+++ fasum/lib/screens/add_post_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
@@ -33,6 +35,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
   }
 
   Future<void> _compressAndEncodeImage() async {
+    if(_image == null)return;
     final compressedImage = await FlutterImageCompress.compressWithFile(
       _image!.path,
       quality: 50,
@@ -58,6 +61,57 @@ class _AddPostScreenState extends State<AddPostScreen> {
       if (permission == LocationPermission.deniedForever ||
           permission == LocationPermission.denied) {
         throw Exception('Location Permissions are denied.');
+      }
+    }
+    try {
+      final Position = await Geolocator.getCurrentPosition(
+        locationSettings: LocationSettings(accuracy: LocationAccuracy.high),
+      ).timeout(const Duration(seconds: 10));
+      setState(() {
+        _latitude = null;
+        _longitude = null;
+      });
+    }
+    Future<void> _submitPost() async{
+      if (_base64Image == null || _descriptionController.text.isEmpty)return;
+      setState(() => _isUploading = true); {
+        final now = DateTime.now().toIso8601String();
+        final uid = FirebaseAuth.instance.currentUser?.uid;
+        if (uid == null) {
+          setState(() => _isUploading=false); {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('User Not Found.')),);
+          };
+          return;
+        }
+        try{
+          await _getLocation();
+          //Ambil Nama Lengkap dari koleksi Users
+          final userDoc=
+          await FirebaseFirestore.instance.collection('users').doc(uid).get();
+          final fullName = userDoc.data()?['FullName']?? 'Anonymous';
+          await FirebaseFirestore.instance.collection('posts').add({
+            'image' : _base64Image,
+            'description':_descriptionController,
+            'createdAt':now,
+            'latitude': _latitude,
+            "longitude" : _longitude,
+            'fullName' : fullName,
+            'userId':uid,
+          });
+          if (!mounted) return; 
+            Navigator.pop(context);
+        }catch(e){
+          debugPrint('Upload failed :$e');
+          if (!mounted)return;
+          setState(() => _isUploading=false); {
+            ScaffoldMessenger.of(context).showSnackBar(snackBar(context : Text('Failed to upload the post')),);
+          };
+        }
+      void _showImageSourceDialog(){
+        showDialog(context: context, builder: (context))
+      }
+      }; {
+        
       }
     }
   }
